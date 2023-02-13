6c776f0f1fe5bfaf679f0e2acd38930b4c4c22fa
Ezra Sharp <ezra.sharp@nice.kiwi>
2023-02-13 22:58:18 +1300

    Fix missing alpine versions and wrong env var

--- build.sh
+++ build.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
-
 docker buildx create --name phpbuilder --use 
 
 # Build PHP 8
-PHP_VERSION=8.0 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.0 \
+    --build-arg ALPINE_VERSION=3.16 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.0 \
     --file Dockerfile \
     .
 
-PHP_VERSION=8.0.25 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.0.25 \
+    --build-arg ALPINE_VERSION=3.15 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.0.25 \
     --file Dockerfile \
     .
 
-PHP_VERSION=8.0.26 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.0.26 \
+    --build-arg ALPINE_VERSION=3.16 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.0.26 \
     --file Dockerfile \
     .
 
+docker buildx build \
+    --build-arg PHP_VERSION=8.0.27 \
+    --build-arg ALPINE_VERSION=3.16 \
+    --platform linux/amd64,linux/arm64 \
+    --push \
+    --tag nicekiwi/php:8.0.27 \
+    --file Dockerfile \
+    .
+
 # Build PHP 8.1
-PHP_VERSION=8.1 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.1 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.1 \
     --file Dockerfile \
     .
 
-PHP_VERSION=8.1.15 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.1.15 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.1.15 \
@@ -40,14 +56,16 @@ PHP_VERSION=8.1.15 docker buildx build \
     .
 
 # Build PHP 8.2
-PHP_VERSION=8.2 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.2 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.2 \
     --file Dockerfile \
     .
 
-PHP_VERSION=8.2.2 docker buildx build \
+docker buildx build \
+    --build-arg PHP_VERSION=8.2.2 \
     --platform linux/amd64,linux/arm64 \
     --push \
     --tag nicekiwi/php:8.2.2 \
